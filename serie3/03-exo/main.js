2463f01e5bf9ffa91129ccebdfe94bc9061b9ed9
Naïké Dramé <98624859+coolboogie90@users.noreply.github.com>
2023-09-27 15:08:04 +0200

    Solved serie3-03

---
+++ serie3/03-exo/main.js
@@ -0,0 +1,40 @@
+/*
+Carlos is a huge fan of something he calls smooth sentences.
+
+A smooth sentence is one where the last letter of each word is identical to the first letter the following word (and not case sensitive, so "A" would be the same as "a").
+
+The following would be a smooth sentence "Carlos swam masterfully" because "Carlos" ends with an "s" and swam begins with an "s" and swam ends with an "m" and masterfully begins with an "m".
+
+Create a function that determines whether the input sentence is a smooth sentence, returning a boolean value true if it is, false if it is not.
+
+Notes
+The last and first letters are case insensitive.
+There will be no punctuation in each sentence.
+
+Expected results
+isSmooth("Marta appreciated deep perpendicular right trapezoids") ➞ true
+isSmooth("Someone is outside the doorway") ➞ false
+isSmooth("She eats super righteously") ➞ true
+*/
+
+function isSmooth(str) {
+    let arr = str.toLowerCase().split(' ');
+    console.log(arr);
+    for (let i = 0; i < arr.length - 1; i++) {
+        const currentWord = arr[i];
+        const nextWord = arr[i + 1];
+        if (currentWord[currentWord.length - 1] !== nextWord[0]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isSmooth("Marta appreciated deep perpendicular right trapezoids"));
+console.log(isSmooth("Someone is outside the doorway"));
+console.log(isSmooth("She eats super righteously"));
+console.log(isSmooth("Carlos swam masterfully"));
+console.log(isSmooth("Carlos swam masterfully"));
+
+/*const row = haystack.findIndex(row => row.includes(search));
+const col = haystack[row].indexOf(search);*/
