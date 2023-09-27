a3b1a503d55cf85af8d12d298cd9e732573a2c02
Naïké Dramé <98624859+coolboogie90@users.noreply.github.com>
2023-09-27 11:32:40 +0200

    Solved serie3-00

---
+++ serie3/00-exo/main.js
@@ -0,0 +1,33 @@
+/*
+Create a function that determines whether a number is Oddish or Evenish. A number is Oddish if the sum of all of its digits is odd, and a number is Evenish if the sum of all of its digits is even. If a number is Oddish, return "Oddish". Otherwise, return "Evenish".
+
+For example, oddishOrEvenish(121) should return "Evenish", since 1 + 2 + 1 = 4. oddishOrEvenish(41) should return "Oddish", since 4 + 1 = 5.
+
+Expected results
+oddishOrEvenish(43) ➞ "Oddish"
+// 4 + 3 = 7
+// 7 % 2 = 1
+
+oddishOrEvenish(373) ➞ "Oddish"
+// 3 + 7 + 3 = 13
+// 13 % 2 = 1
+
+oddishOrEvenish(4433) ➞ "Evenish"
+// 4 + 4 + 3 + 3 = 14
+// 14 % 2 = 0
+*/
+
+function oddishOrEvenish(n) {
+    let intArr = n.toString().split('').map(Number).reduceRight();
+    let sum = 0;
+    for (let i of intArr) {
+        sum += i;
+    }
+    let result = sum % 2 === 0 ? "Evenish" : "Oddish";
+    return result;
+}
+
+console.log(oddishOrEvenish(43));
+console.log(oddishOrEvenish(373));
+console.log(oddishOrEvenish(4433));
+console.log(oddishOrEvenish(445984565433));
