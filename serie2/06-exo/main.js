6a4788b96693a13f6502c64243f91f2fef4134e6
Naïké Dramé <98624859+coolboogie90@users.noreply.github.com>
2023-09-27 09:07:47 +0200

    Solved serie2-06

--- serie2/06-exo/main.js
+++ serie2/06-exo/main.js
@@ -1,22 +1,46 @@
 /*
-Create a function that takes two arguments. Both arguments are integers, a and b. Return true if one of them is 10 or if their sum is 10.
+In this challenge you will be given an array similar to the following:
+[[3], 4, [2], [5], 1, 6]
+
+In words, elements of the array are either an integer or an array containing a single integer. We humans can clearly see that this array can reasonably be sorted according to "the content of the elements" as:
+[1, [2], [3], 4, [5], 6]
+
+Create a function that, given an array similar to the above, sorts the array according to the "content of the elements".
+
 Notes
-Don't forget to return the result.
+To reiterate, elements of the array will be either integers or arrays with a single integer.
 
 Expected results
-makesTen(9, 10) ➞ true
-makesTen(9, 9) ➞ false
-makesTen(1, 9) ➞ true
+sortIt([4, 1, 3]) ➞ [1, 3, 4]
+sortIt([[4], [1], [3]]) ➞ [[1], [3], [4]]
+sortIt([4, [1], 3]) ➞ [[1], 3, 4]
+sortIt([[4], 1, [3]]) ➞ [1, [3], [4]]
+sortIt([[3], 4, [2], [5], 1, 6]) ➞ [1, [2], [3], 4, [5], 6]
 */
 
-function makesTen(num1, num2) {
-    if(num1 === 10 || num2 ===10 || num1 + num2 === 10) {
-        return true;
-    } else { return false; }
+function sortIt(arr) {
+    arr.sort(function (a, b) {
+        // Check if the elements are arrays or numbers
+        if (Array.isArray(a) && Array.isArray(b)) {
+            // Compare the single integer inside the arrays
+            return a[0] - b[0];
+        } else if (Array.isArray(a)) {
+            // Place arrays before numbers
+            return -1;
+        } else if (Array.isArray(b)) {
+            // Place numbers after arrays
+            return 1;
+        } else {
+            // Compare the numbers directly
+            return a - b;
+        }
+    });
+
+    return arr;
 }
 
-console.log(makesTen(5, 4));
-console.log(makesTen(0, 10));
-console.log(makesTen(6, 4));
-console.log(makesTen(7, 2));
-console.log(makesTen(10, 0));
+console.log(sortIt([4, 1, 3]));
+console.log(sortIt([[4], [1], [3]]))
+console.log(sortIt([4, [1], 3]));
+console.log(sortIt([[4], 1, [3]]));
+console.log(sortIt([[3], 4, [2], [5], 1, 6]));
